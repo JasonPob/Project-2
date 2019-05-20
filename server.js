dfdf36b7ebf774e4960dcd6998d740e4ee10a233
JasonPob <jasonpob@gmail.com>
2019-05-20 14:03:54 -0400

    hope this works

---
+++ server.js
@@ -0,0 +1,58 @@
+// npm install express --save
+// npm install sequelize --save
+// npm install mysql --save
+// npm install passport --save
+// npm install passport-local --save
+// npm install body-parser --save
+// npm install express-session --save
+// npm install bcrypt-nodejs --save
+// npm install express-handlebars --save
+
+var express = require('express');
+var app = express();
+var passport   = require('passport');
+var session    = require('express-session');
+var bodyParser = require('body-parser');
+var env = require('dotenv');
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+ 
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+ 
+app.use(passport.initialize());
+ 
+app.use(passport.session()); // persistent login sessions
+ 
+ 
+app.get('/', function(req, res) {
+ 
+    res.send('Welcome to Passport with Sequelize');
+ 
+});
+ 
+ 
+app.listen(5000, function(err) {
+ 
+    if (!err)
+        console.log("Site is live");
+    else console.log(err)
+ 
+});
+
+//Models
+var models = require("./app/models");
+ 
+//Sync Database
+models.sequelize.sync().then(function() {
+ 
+    console.log('Nice! Database looks fine')
+ 
+}).catch(function(err) {
+ 
+    console.log(err, "Something went wrong with the Database Update!")
+ 
+});
